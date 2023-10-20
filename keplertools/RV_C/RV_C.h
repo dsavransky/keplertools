bae25045b0dd58ff0271be06a4b15768708f1240
Corey Spohn <cas584@cornell.edu>
2023-10-20 13:44:34 -0400

    Fixing header file

--- keplertools/RV_C/RV_C.h
+++ keplertools/RV_C/RV_C.h
@@ -1,2 +1,5 @@
 void meananom(double M[], double t[], double tp, double per, double twopi,
               int n);
+
+void RV_from_time(double rv[], double t[], double tp[], double per[],
+                  double e[], double w[], double K[], int n, int m);
