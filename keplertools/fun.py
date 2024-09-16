8609b80073649b40b94e7814005eedf151353692
Dmitry Savransky <dsavransky@gmail.com>
2024-09-16 15:10:32 -0400

    adding float_ to float64 alias to ensure numpy 2.0 compatibility

--- keplertools/fun.py
+++ keplertools/fun.py
@@ -4,6 +4,7 @@ from typing import Optional, Tuple, Union
 import numpy as np
 import numpy.typing as npt
 
+np.float_ = np.float64  # for numpy 2 compatibility
 floatORarray = Union[float, npt.NDArray[np.float_]]
 
 try:
